ca0f36a3d95fa9ad0e72d2c4bbf5169b5bdade1d
dragon.mok <priclessmok@gmail.com>
2017-07-03 21:36:43 +0900

    Initial commit

---
+++ app/src/main/java/com/cse1/lym/a17_svac_droid_first/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cse1.lym.a17_svac_droid_first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
